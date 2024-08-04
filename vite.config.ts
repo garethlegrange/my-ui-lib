530956d47624cb94a642db3a77d36c70d64a067b
Gareth le Grange <garethlegrange@gmail.com>
2024-08-04 15:06:37 +0200

    Setup css for export

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(), tsconfigPaths()],
+  plugins: [
+    react(),
+    dts({
+      include: ["lib"],
+    }),
+    tsconfigPaths(),
+    libInjectCss(),
+  ],
   build: {
     lib: {
+      formats: ["es"],
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "lib/main.ts"),
+      // entry: resolve(__dirname, "lib/main.ts"),
+      entry: {
+        main: resolve(__dirname, "lib/main.ts"),
+        button: "lib/components/button/index.tsx",
+        switch: "lib/components/switch/index.tsx",
+      },
       name: "SednaUI",
       // the proper extensions will be added
       fileName: "sedna-ui",
@@ -20,6 +34,11 @@ export default defineConfig({
       // into your library
       external: ["react", "react-dom"],
       output: {
+        // Put chunk files at <output>/chunks
+        chunkFileNames: "chunks/[name].[hash].js",
+        // Put chunk styles at <output>/assets
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
